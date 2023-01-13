2b16ec9c994fd9cf2d5693b5866e6bdfbf21b371
Miles-Cownie <miles_cownie94@protonmail.com>
2023-01-13 10:51:41 +0000

    Add stripe elements and link to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MPkJnLDCLIrJ0jdixUT2ssUCHM5rCi66OsVmb7tQpPxc9mWB9U1RwxryhVMBXtOFCMwrxYjWDRcLKXDYwji7Gjp00sY53CM0Z',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
